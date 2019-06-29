6d431d9bc340f2365f1a096832424c2eed97700d
Fumiko Toyoda <toyodafumiko@gmail.com>
2019-06-29 20:57:09 +0900

    adding components to the LP

--- src/components/header.js
+++ src/components/header.js
@@ -5,13 +5,12 @@ import './Header.css'
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
-  <div className="HeaderGroup">
-    <img width = "30"  src = {require('../images/logo-designcode.svg')} />
-    <Link to="/"><img width="30" /></Link>
-    <Link to="/courses">Courses</Link>
-    <Link to="/downloads">Downloads</Link>
-    <Link to="/workshops">Workshops</Link>
-    <Link to="/buy"><button>Buy</button></Link>
+    <div className="HeaderGroup">
+      <Link to="/"><img src={require('../images/logo-designcode.svg')}  width="30" /></Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy"><button>Buy</button></Link>
   </div>
 </div>
 )
